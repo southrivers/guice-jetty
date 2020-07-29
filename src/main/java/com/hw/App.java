762005f9494b8e8958b9a5400ff024f0a2d3871b
southrivers <1191978138@qq.com>
2020-07-30 02:25:02 +0800

    解决请求路径找不到的问题

--- src/main/java/com/hw/App.java
+++ src/main/java/com/hw/App.java
@@ -1,7 +1,12 @@
 package com.hw;
 
+import com.google.inject.servlet.GuiceFilter;
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.DefaultServlet;
 import org.eclipse.jetty.servlet.ServletContextHandler;
+
+import javax.servlet.DispatcherType;
+import java.util.EnumSet;
 
 /**
  * Hello world!
@@ -12,9 +17,12 @@ public class App {
         Server server = new Server(8090);
 
         ServletContextHandler servletContextHandler = new ServletContextHandler();
-        servletContextHandler.setContextPath("/*");
+        servletContextHandler.setContextPath("/");
         servletContextHandler.addEventListener(new ContextListener());
 
+        // TODO 这一步是必须的，必须添加拦截所有的请求，否则就无法拦截到指定的请求，原因尚不清楚
+        servletContextHandler.addFilter(GuiceFilter.class, "/*",
+                EnumSet.allOf(DispatcherType.class));
         server.setHandler(servletContextHandler);
         server.start();
         server.join();
